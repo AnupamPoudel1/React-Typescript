824b301169d1eaa97b16398cadd5f7c8ff8098a4
Anupam <anupampoudel78@gmail.com>
2024-08-02 13:06:03 +0545

    Using Type to define props

--- src/learnings/02-props/index.tsx
+++ src/learnings/02-props/index.tsx
@@ -1,12 +1,18 @@
+type ComponentProps = {
+    name: string, 
+    id: number
+};
 
-const index = ({ name, id }: { name: string; id: number }): JSX.Element => {
+const index = (props: ComponentProps): JSX.Element => {
+    // We can also do ({name, id}: ComponentProps)
+    // Either to destructure the props or not is entirely our choice
     return (
         <div>
             <h1>React Typescript</h1>
             <p>Props</p>
             <div>
-                <p>Id: {id}</p>
-                <p>Name: {name}</p>
+                <p>Id: {props.id}</p>
+                <p>Name: {props.name}</p>
             </div>
         </div>
     )
