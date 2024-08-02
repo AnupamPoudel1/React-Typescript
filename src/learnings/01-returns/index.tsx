a27f7398d6b0d3b089fa433479f2e870ca57edc4
Anupam <anupampoudel78@gmail.com>
2024-08-02 12:35:08 +0545

    Return Types in TSX

--- src/learnings/01-returns/index.tsx
+++ src/learnings/01-returns/index.tsx
@@ -1,16 +1,16 @@
 
 const index = (): JSX.Element | string | null => {
 
+    return (
+        <div>
+            <h1>React Typescript</h1>
+            <p>Return Types</p>
+        </div>
+    )
+
     return 'Hello'
 
     return null
-
-    return (
-        <div>
-            <h1>This is a TSX Element</h1>
-            <p>It has return type of JSX.Element</p>
-        </div>
-    )
 }
 
 export default index
