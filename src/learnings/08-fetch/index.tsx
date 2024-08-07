10471814c21b22944e9745256769c514e06b0e5d
Anupam <anupampoudel78@gmail.com>
2024-08-07 22:18:55 +0545

    React Typescript axios fetch and react query examples

--- src/learnings/08-fetch/index.tsx
+++ src/learnings/08-fetch/index.tsx
@@ -1,50 +1,30 @@
-import { useState, useEffect } from "react";
-import { type Tour, tourSchema } from './types';
-
-const url = 'https://www.course-api.com/react-tours-project';
+import { fetchData } from "./types";
+import { useQuery } from "@tanstack/react-query";
 
 const index = () => {
 
-    const [data, setData] = useState<Tour[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [isError, setIsError] = useState<string | null>(null);
+    const {
+        isPending,
+        isError,
+        error,
+        data: responses,
+    } = useQuery({
+        queryKey: ['data'],
+        queryFn: fetchData
+    });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            try {
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error('Failed To Fetch Data.....');
-                }
-                const rawData: Tour[] = await response.json();
-                const result = tourSchema.array().safeParse(rawData);
-                if (!result.success) {
-                    console.log(result.error.message);
-                    throw new Error('Failed To Parse Data.....');
-                }
-                setData(result.data);
-            } catch (error) {
-                const message = error instanceof Error ? error.message : 'Unknown Error Occured.....'
-                setIsError(message);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchData();
-    }, [])
-
-    if (isLoading) {
+    if (isPending) {
         return (
             <div className='flex flex-col justify-center items-center bg-gray-200 p-10'>
                 <p className="font-bold text-xl">Loading .....</p>
             </div>
         )
     }
+
     if (isError) {
         return (
             <div className='flex flex-col justify-center items-center bg-gray-200 p-10'>
-                <p className="font-bold text-xl">Error: {isError}</p>
+                <p className="font-bold text-xl">Error: {error.message}</p>
             </div>
         )
     }
@@ -53,10 +33,10 @@ const index = () => {
         <div className='flex flex-col justify-center items-center bg-gray-200 p-10'>
             <p className="font-bold text-xl">Fetch</p>
             <div className="m-2 p-2">
-                {data.map((data) => {
+                {responses.map((response) => {
                     return (
-                        <p className="font-normal text-base m-2" key={data.id}>
-                            Name: {data.name}
+                        <p className="font-normal text-base m-2" key={response.id}>
+                            Name: {response.name}
                         </p>
                     )
                 })}
--- src/learnings/08-fetch/types.ts
+++ src/learnings/08-fetch/types.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { z } from "zod";
+
+const url = 'https://www.course-api.com/react-tours-project';
 
 export const tourSchema = z.object({
     id: z.string(),
@@ -9,3 +12,14 @@ export const tourSchema = z.object({
 });
 
 export type Tour = z.infer<typeof tourSchema>
+
+export const fetchData = async (): Promise<Tour[]> => {
+    const response = await axios.get<Tour[]>(url);
+    const result = tourSchema.array().safeParse(response.data);
+
+    if (!result.success) {
+        console.log(result.error.message);
+        throw new Error('Failed To Parse Data.....');
+    }
+    return result.data;
+}
