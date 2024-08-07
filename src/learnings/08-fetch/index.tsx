2cf6588b3f0c8d9dead87bfdf95bdbd9b6a8c521
Anupam <anupampoudel78@gmail.com>
2024-08-07 21:41:25 +0545

    Fetch API using ZOD library in React Typescript

--- src/learnings/08-fetch/index.tsx
+++ src/learnings/08-fetch/index.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { type Tour, tourSchema } from './types';
 
 const url = 'https://www.course-api.com/react-tours-project';
 
 const index = () => {
 
+    const [data, setData] = useState<Tour[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [isError, setIsError] = useState<string | null>(null);
 
@@ -15,10 +17,15 @@ const index = () => {
                 if (!response.ok) {
                     throw new Error('Failed To Fetch Data.....');
                 }
-                const rawData = await response.json();
-                console.log(rawData);
+                const rawData: Tour[] = await response.json();
+                const result = tourSchema.array().safeParse(rawData);
+                if (!result.success) {
+                    console.log(result.error.message);
+                    throw new Error('Failed To Parse Data.....');
+                }
+                setData(result.data);
             } catch (error) {
-                const message = error instanceof Error ? error.message : 'Unknown Error Occured .....'
+                const message = error instanceof Error ? error.message : 'Unknown Error Occured.....'
                 setIsError(message);
             } finally {
                 setIsLoading(false);
@@ -44,7 +51,16 @@ const index = () => {
 
     return (
         <div className='flex flex-col justify-center items-center bg-gray-200 p-10'>
-            <p className="font-bold text-xl">Reducers</p>
+            <p className="font-bold text-xl">Fetch</p>
+            <div className="m-2 p-2">
+                {data.map((data) => {
+                    return (
+                        <p className="font-normal text-base m-2" key={data.id}>
+                            Name: {data.name}
+                        </p>
+                    )
+                })}
+            </div>
         </div>
     )
 }
