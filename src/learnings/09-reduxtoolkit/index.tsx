5b381d603bfebc0fa66d6969194e999a76921e0d
Anupam <anupampoudel78@gmail.com>
2024-08-08 21:11:26 +0545

    React Typescript Redux Toolkit

--- src/learnings/09-reduxtoolkit/index.tsx
+++ src/learnings/09-reduxtoolkit/index.tsx
@@ -2,7 +2,7 @@
 const index = () => {
     return (
         <div className='flex flex-col justify-center items-center bg-gray-100 p-10'>
-            <p className="font-bold text-xl">Redux-Toolkit</p>
+            <p className="font-bold text-xl">Redux-Toolkit Typescript</p>
         </div>
     )
 }
